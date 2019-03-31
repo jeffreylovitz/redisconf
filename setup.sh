8cbfa4263790db8ccd46c0e17c4b88e6c96ba730
Jeffrey Lovitz <jeffrey.lovitz@gmail.com>
2019-03-31 13:42:46 -0400

    Add pip user flag

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pip install -r requirements.txt
+pip install --user -r requirements.txt
 python bulk_insert.py worldbuilding -s -q -n inputs/User.csv -n inputs/Question.csv -n inputs/Tag.csv -n inputs/Answer.csv -r inputs/ANSWER_TO.csv -r inputs/AUTHOR_OF.csv -r inputs/HAS_TAG.csv
 redis-cli save
